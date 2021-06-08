d9a37e7a6f50dc75e3d3ee369e20c892b6b721e3
sefutsu <sefutsu.r@gmail.com>
2021-06-08 16:43:44 +0900

    output fib.v

--- src/cdfg/output.rs
+++ src/cdfg/output.rs
@@ -32,7 +32,7 @@ impl fmt::Display for Function {
     write!(f, "always @(posedge ap_clk) begin\n")?;
     write!(f, "\tif(ap_rst) begin\n")?;
     for r in self.regs.iter() {
-      write!(f, "\t\t{} <= 32'h{}\n", r.name, r.ini)?;
+      write!(f, "\t\t{} <= {}\n", r.name, r.ini)?;
     }
     write!(f, "\t\tap_done <= 0;\n\t\tap_ready <= 1;\n\t\tap_return <= 0;\n\t\tap_fsm <= 0;\n")?;
     write!(f, "\tend else begin\n")?;
@@ -59,6 +59,81 @@ impl fmt::Display for Function {
 
 impl fmt::Display for CDFGNode {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    write!(f, "{}\n", self.idx)
+    write!(f, "\t\t\t{}: begin\n", self.idx)?;
+    write!(f, "{}", self.src)?;
+    write!(f, "{}", self.next)?;
+    write!(f, "\t\t\tend\n")
+  }
+}
+
+impl fmt::Display for ast::Stat {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Self::Void => (),
+      Self::Expression(e) => write!(f, "\t\t\t\t{};\n", e)?,
+      Self::Compound(v) => {
+        for s in v.iter() {
+          write!(f, "{}", s)?;
+        }
+      },
+      Self::Return(e) => {
+        write!(f, "\t\t\t\tap_return <= {}\n", e)?;
+        write!(f, "\t\t\t\tap_ready <= 1;\n\t\t\t\tap_done <= 1;\n")?;
+      },
+      _ => unreachable!(),
+    }
+    Ok(())
+  }
+}
+
+impl fmt::Display for ast::Expr {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Self::Int(i) => write!(f, "{}", i),
+      Self::Id(s) => write!(f, "{}", s),
+      Self::Op1(op, e) => write!(f, "{}({})", op, e),
+      Self::Op2(op, e1, e2) => write!(f, "({}) {} ({})", e1, op, e2),
+      Self::Assign(s, e) => write!(f, "{} = {}", s, e),
+    }
+  }
+}
+
+impl fmt::Display for Next {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Self::Jump(i) => write!(f, "\t\t\t\tap_fsm <= {};\n", i),
+      Self::Branch(e, x, y) => write!(f, "\t\t\t\tif({}) begin\n\t\t\t\t\tap_fsm <= {};\n\t\t\t\tend else begin\n\t\t\t\t\tap_fsm <= {};\n\t\t\t\tend\n", e, x, y)
+    }
+  }
+}
+
+impl fmt::Display for ast::Op1 {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Self::Neg => write!(f, "-"),
+      Self::Lognot => write!(f, "!")
+    }
+  }
+}
+
+impl fmt::Display for ast::Op2 {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Self::Add => write!(f, "+"),
+      Self::Sub => write!(f, "-"),
+      Self::Mul => write!(f, "*"),
+      Self::Div => write!(f, "/"),
+      Self::And => write!(f, "&"),
+      Self::Or => write!(f, "|"),
+      Self::Xor => write!(f, "^"),
+      Self::Logand => write!(f, "&&"),
+      Self::Logor => write!(f, "||"),
+      Self::Eq => write!(f, "=="),
+      Self::Ne => write!(f, "!="),
+      Self::Lt => write!(f, "<"),
+      Self::Le => write!(f, "<="),
+      Self::Lshift => write!(f, "<<<"),
+      Self::Rshift => write!(f, ">>>"),
+    }
   }
 }
---
+++ test/fib.v
@@ -0,0 +1,96 @@
+module fib (
+	input ap_clk,
+	input ap_rst,
+	input ap_start,
+	output reg ap_done,
+	output ap_idle,
+	output reg ap_ready,
+	input [31:0] ap_n,
+	output reg [31:0] ap_return
+);
+
+reg [31:0] n;
+reg [31:0] a;
+reg [31:0] b;
+reg [31:0] tmp;
+
+reg [3:0] ap_fsm
+assign ap_idle = ap_fsm == 0;
+
+always @(posedge ap_clk) begin
+	if(ap_rst) begin
+		n <= 0
+		a <= 0
+		b <= 1
+		tmp <= 0
+		ap_done <= 0;
+		ap_ready <= 1;
+		ap_return <= 0;
+		ap_fsm <= 0;
+	end else begin
+		case(ap_fsm)
+			0: begin
+				if(ap_start) begin
+					n <= ap_n;
+					ap_ready <= 0;
+					ap_done <= 0;
+					ap_fsm <= 1;
+			1: begin
+				if((n) < (2)) begin
+					ap_fsm <= 2;
+				end else begin
+					ap_fsm <= 4;
+				end
+			end
+			2: begin
+				ap_return <= n
+				ap_ready <= 1;
+				ap_done <= 1;
+				ap_fsm <= 0;
+			end
+			3: begin
+				ap_fsm <= 4;
+			end
+			4: begin
+				if((1) < (n)) begin
+					ap_fsm <= 5;
+				end else begin
+					ap_fsm <= 11;
+				end
+			end
+			5: begin
+				a = (a) + (b);
+				ap_fsm <= 6;
+			end
+			6: begin
+				tmp = a;
+				ap_fsm <= 7;
+			end
+			7: begin
+				a = b;
+				ap_fsm <= 8;
+			end
+			8: begin
+				b = tmp;
+				ap_fsm <= 9;
+			end
+			9: begin
+				n = (n) - (1);
+				ap_fsm <= 10;
+			end
+			10: begin
+				ap_fsm <= 4;
+			end
+			11: begin
+				ap_return <= b
+				ap_ready <= 1;
+				ap_done <= 1;
+				ap_fsm <= 0;
+			end
+			default: begin
+				ap_fsm <= 0;
+			end
+		endcase
+	end
+end
+endmodule
